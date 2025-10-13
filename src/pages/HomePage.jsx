6c10482a0c3b60c13d069decc240b7e7455ea3d1
Yejun Guan <guanyejun79@gmail.com>
2025-10-13 19:48:27 +0100

    update: fetch data from abstract api,add loading animation and wired Header component into the homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,45 @@
+import {useEffect, useState} from "react";
+import abstractGeolocationApi from "../services/abstractGeolocationApi.js";
+import Lottie from "lottie-react";
+import LoadingAnimation from "../assets/loading-animation.json";
+import Header from "../components/Header.jsx";
+
 export default function HomePage(){
+    const [loading, setLoading] = useState(true);
+    const [currentWeatherData, setCurrentWeatherData] = useState(null);
+    const [isCelsius,setIsCelsius] = useState(true);
+
+
+    useEffect(() => {
+        setLoading(true);
+        abstractGeolocationApi().then((response) => {
+            setCurrentWeatherData(response);
+        }).catch((error) => {
+            console.log(error);
+        }).finally(
+            () => setLoading(false)
+        )
+
+    },[]);
+
+    useEffect(() => {
+
+    },[currentWeatherData]);
+
+    if (loading) {
+        return (
+            <div className="fixed inset-0 z-50 bg-white">
+                <div className="w-[550px] h-[550px] mt-48 mx-auto">
+                    <Lottie animationData={LoadingAnimation} loop={true} />
+                </div>
+            </div>
+        )
+    }
+
     return (
         <>
             <section className="rounded-xl border border-slate-400 bg-white shadow-sm overflow-hidden md:w-1/2 lg:w-1/3 mt-8 mx-auto">
-
+                <Header region={currentWeatherData.region} country={currentWeatherData.country} isCelsius={isCelsius} />
             </section>
         </>
     )
