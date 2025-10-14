b123ad6d0cdae946047815bf5924367472f153eb
Yejun Guan <guanyejun79@gmail.com>
2025-10-14 17:00:47 +0100

    Fix: check if props are null before display to aviod null error

--- src/components/DetailCard.jsx
+++ src/components/DetailCard.jsx
@@ -1,44 +1,69 @@
 import celsiusToFahrenheit from "../utils/celsiusToFahrenheit.js";
 
 export default function DetailsCard({currentWeatherData,displayWeatherData,isFutureDateSelected,isCelsius}) {
-    const {city} = currentWeatherData;
-    const {currentTime,emoji,currentTemperature,description,windSpeed,
-        uxIndex,sunset,feelLike,sunrise,relativeHumidity,precipitationProbability} =displayWeatherData;
+
     return (
         <div className="p-5 bg-white">
             <div className="flex items-center justify-between">
                 <div>
-                    <h2 className="text-xl font-semibold text-gray-800">{city}</h2>
-                    <p className="text-sm text-gray-500 mt-2">{currentTime}</p>
+                    <h2 className="text-xl font-semibold text-gray-800">{currentWeatherData?.city&& (
+                        currentWeatherData?.city
+                    )}</h2>
+                    <p className="text-sm text-gray-500 mt-2">{displayWeatherData?.currentTime&& (
+                        displayWeatherData.currentTime
+                    )}
+                    </p>
                 </div>
-                <div className="text-6xl">{emoji}</div>
+                <div className="text-6xl">{displayWeatherData?.emoji && (
+                    displayWeatherData.emoji
+                )}</div>
             </div>
 
             <div className="mt-3 flex items-end gap-2">
                 <p className="text-4xl font-bold text-gray-900">
-                    {isCelsius ? currentTemperature : celsiusToFahrenheit(currentTemperature)}°
+                    {displayWeatherData?.currentTemperature && (
+                        isCelsius ? displayWeatherData.currentTemperature : celsiusToFahrenheit(displayWeatherData.currentTemperature)
+                    )}°
                 </p>
-                <p className="text-sm text-gray-600 mb-1">{description}</p>
+                <p className={`text-4xl font-bold text-gray-900 ${isFutureDateSelected ? "block" : "hidden" }`}> / {displayWeatherData?.temperatureMin && (
+                    isCelsius ? displayWeatherData.temperatureMin : celsiusToFahrenheit(displayWeatherData.temperatureMin)
+                )}°</p>
+                <p className="text-sm text-gray-600 mb-1">{displayWeatherData?.description && (
+                    displayWeatherData.description
+                )}</p>
             </div>
 
             <div className="mt-5 grid grid-cols-2 gap-y-3 text-sm text-gray-700">
                 <div className={"flex items-center gap-2 "}>
-                    🌡️ <span>Feels Like {isCelsius ? feelLike : celsiusToFahrenheit(feelLike)}°</span>
+                    🌡️ <span>Feels Like {displayWeatherData?.feelLike && (
+                    isCelsius ? displayWeatherData.feelLike : celsiusToFahrenheit(displayWeatherData.feelLike))}°</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    💨 <span>Wind Speed {windSpeed} km/h</span>
+                    💨 <span>Wind Speed {displayWeatherData?.windSpeed && (
+                    displayWeatherData.windSpeed
+                )} km/h</span>
                 </div>
                 <div className={"flex items-center gap-2"}>
-                    💧 <span>Humidity  {relativeHumidity}%</span>
+                    💧 <span>Humidity  {displayWeatherData?.relativeHumidity && (
+                    displayWeatherData.relativeHumidity
+                )}%</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    🌦️ <span>Precipitation Probability  {precipitationProbability}%</span>
+                    🌦️ <span>Precipitation Probability  {displayWeatherData?.precipitationProbability && (
+                    displayWeatherData.precipitationProbability
+                )}%</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    🌅 <span>Sunrise Time {sunrise} / Sunset Time {sunset}</span>
+                    🌅 <span>Sunrise Time {displayWeatherData?.sunrise && (
+                    displayWeatherData.sunrise
+                )}  / Sunset Time {displayWeatherData?.sunset && (
+                    displayWeatherData.sunset
+                )}</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    🌞 <span> UX Index {uxIndex}</span>
+                    🌞 <span> UX Index {displayWeatherData?.uxIndex && (
+                    displayWeatherData.uxIndex
+                )}</span>
                 </div>
             </div>
         </div>
